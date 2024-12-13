768f58f854eda6af3ed3ff6f59dc0e4906e33999
polancodf2024 <polanco@unam.mx>
2024-12-13 11:53:09 -0600

    Add files via upload

--- cuestionario.biobanco17.py
+++ cuestionario.biobanco17.py
@@ -5,7 +5,6 @@ import pandas as pd
 from datetime import datetime
 from filelock import FileLock
 
-
 # Conectar a la base de datos SQLite
 conn = sqlite3.connect('identificaciones.db')
 cursor = conn.cursor()
@@ -26,7 +25,6 @@ def generar_identificacion(prefijo):
     identificacion = f"{prefijo}{nuevo_id:06d}"
     return identificacion
 
-
 # Mostrar el logo
 st.image("escudo_COLOR.jpg", width=100)
 
@@ -40,30 +38,20 @@ responses = {}
 # Título del cuestionario
 st.title('Cuestionario Paciente - BioBanco')
 
+# Lista de estados de México
+estados_mexico = [
+    'Otro', 'Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua',
+    'Ciudad de Mexico', 'Coahuila', 'Colima', 'Durango', 'Estado de Mexico', 'Guanajuato', 'Guerrero', 
+    'Hidalgo', 'Jalisco', 'Michoacan', 'Morelos', 'Nayarit', 'Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro',
+    'Quintana Roo', 'San Luis Potosi', 'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz',
+    'Yucatan', 'Zacatecas'
+]
+
 # Formulario para todas las preguntas
 with st.form(key='cuestionario_form'):
     fecha_entrevista = st.date_input('Fecha de entrevista', value=datetime.now())
     responses['Fecha de entrevista'] = fecha_entrevista.strftime('%d/%m/%Y')
 
-    # Inicio con las preguntas de peso, estatura y otros datos biométricos
-    peso = st.number_input('Peso (Kg)', min_value=35.0, max_value=150.0, step=0.1, format="%.1f")
-    responses['Peso (Kg)'] = peso
-
-    estatura = st.number_input('Estatura (m)', min_value=1.20, max_value=2.00, step=0.01, format="%.2f")
-    responses['Estatura (m)'] = estatura
-
-    if estatura > 0:
-        imc = round(peso / (estatura ** 2), 1)
-    else:
-        imc = 0.0
-    responses['Índice de masa corporal (IMC)'] = imc
-
-    responses['Circunferencia de cintura (cm)'] = st.number_input('Circunferencia de cintura (cm)', min_value=50.0, max_value=150.0, step=0.1, format="%.1f")
-    responses['Tensión arterial Sistólica (mmHg)'] = st.number_input('Tensión arterial Sistólica (mmHg)', min_value=50, max_value=220, step=1)
-    responses['Tensión arterial Diastólica (mmHg)'] = st.number_input('Tensión arterial Diastólica (mmHg)', min_value=40, max_value=130, step=1)
-    responses['Frecuencia cardiaca (lpm)'] = st.number_input('Frecuencia cardiaca (lpm)', min_value=40, max_value=120, step=1)
-
-    # Continuación del cuestionario
     responses['Procedencia del paciente'] = st.selectbox(
         'Procedencia del paciente', 
         ['Consulta externa lado A', 'Consulta externa lado B', 'Clínica Arritmias', 'Clínica Coagulación', 
@@ -87,7 +75,31 @@ with st.form(key='cuestionario_form'):
 
     responses['Género'] = st.selectbox('Género', ['Masculino', 'Femenino', 'Otro'])
 
-    # Nueva sección de enfermedades familiares
+    responses['Peso (Kg)'] = st.number_input('Peso (Kg)', min_value=35.0, max_value=150.0, step=0.1, format="%.1f")
+
+    responses['Estatura (m)'] = st.number_input('Estatura (m)', min_value=1.20, max_value=2.00, step=0.01, format="%.2f")
+
+    if responses['Estatura (m)'] > 0:
+        imc = round(responses['Peso (Kg)'] / (responses['Estatura (m)'] ** 2), 1)
+    else:
+        imc = 0.0
+    responses['Índice de masa corporal (IMC)'] = imc
+
+    responses['Circunferencia de cintura (cm)'] = st.number_input('Circunferencia de cintura (cm)', min_value=50.0, max_value=150.0, step=0.1, format="%.1f")
+    responses['Tensión arterial Sistólica (mmHg)'] = st.number_input('Tensión arterial Sistólica (mmHg)', min_value=50, max_value=220, step=1)
+    responses['Tensión arterial Diastólica (mmHg)'] = st.number_input('Tensión arterial Diastólica (mmHg)', min_value=40, max_value=130, step=1)
+    responses['Frecuencia cardiaca (lpm)'] = st.number_input('Frecuencia cardiaca (lpm)', min_value=40, max_value=120, step=1)
+
+    responses['Grupo étnico'] = st.selectbox('Grupo étnico', ['Indígena', 'Mestizo', 'Afrodescendiente', 'Otro'])
+
+    responses['¿Dónde nació su abuelo materno?'] = st.selectbox('¿Dónde nació su abuelo materno?', estados_mexico)
+    responses['¿Dónde nació su abuela materna?'] = st.selectbox('¿Dónde nació su abuela materna?', estados_mexico)
+    responses['¿Dónde nació su abuelo paterno?'] = st.selectbox('¿Dónde nació su abuelo paterno?', estados_mexico)
+    responses['¿Dónde nació su abuela paterna?'] = st.selectbox('¿Dónde nació su abuela paterna?', estados_mexico)
+    responses['¿Dónde nació su padre?'] = st.selectbox('¿Dónde nació su padre?', estados_mexico)
+    responses['¿Dónde nació su madre?'] = st.selectbox('¿Dónde nació su madre?', estados_mexico)
+    responses['¿Dónde nació usted?'] = st.selectbox('¿Dónde nació usted?', estados_mexico)
+
     st.text("¿Tuvo o tiene familiar(es) con alguna de las siguientes enfermedades?")
     enfermedades = [
         'Cardiopatía congénita', 'Angina', 'Valvulopatía', 'Cardiopatía pulmonar',
@@ -96,7 +108,6 @@ with st.form(key='cuestionario_form'):
     ]
     familiares = ['Madre', 'Padre', 'Ambos', 'Hermano(a)', 'Ninguno']
 
-    # Diccionario para almacenar respuestas
     enfermedades_respuestas = {enfermedad: {familiar: False for familiar in familiares} for enfermedad in enfermedades}
 
     for enfermedad in enfermedades:
@@ -107,7 +118,6 @@ with st.form(key='cuestionario_form'):
 
     responses['Familiares con enfermedades específicas'] = enfermedades_respuestas
 
-    # Nueva sección para preguntas con opciones únicas
     st.text("Complete las siguientes preguntas:")
     preguntas = [
         "¿Fuma usted actualmente?",
@@ -117,19 +127,18 @@ with st.form(key='cuestionario_form'):
         "¿Le han indicado medicamento para la diabetes?",
         "¿Tiene dislipidemia?",
         "¿Le han indicado medicamento para la dislipidemia?",
-        "¿Tiene hipertensión?"
+        "¿Tiene hipertensión?",
+        "¿Le han indicado medicamento para la hipertensión?"
     ]
 
     opciones = ['Sí', 'No', 'No sabe']
 
-    # Diccionario para almacenar respuestas de esta sección
     preguntas_respuestas = {}
     for pregunta in preguntas:
         preguntas_respuestas[pregunta] = st.radio(pregunta, opciones, key=pregunta)
 
     responses['Preguntas adicionales'] = preguntas_respuestas
 
-    # Preguntas adicionales proporcionadas
     responses['¿El paciente firmó el consentimiento informado para participar como donador del Biobanco del INCICh?'] = st.selectbox(
         '¿Firmó el paciente el consentimiento informado?', ['Sí', 'No'], key='firma_consentimiento'
     )
@@ -152,7 +161,6 @@ with st.form(key='cuestionario_form'):
     cancel_button = st.form_submit_button(label='Salir sin Guardar')
 
 if submit_button:
-    # Solo generar el número de muestra al hacer clic en "Guardar Respuestas"
     if 'identificacion' not in st.session_state:
         st.session_state.identificacion = generar_identificacion(prefijo)
 
@@ -185,10 +193,8 @@ if cancel_button:
     st.warning('Para salir sin guardar cierre la aplicación.')
     st.stop()
 
-# Solicitar contraseña para descargar el Excel acumulado
 password = st.text_input("Ingrese la contraseña para descargar el archivo:", type="password")
 if password == "tt5plco5":
-    # Mostrar botón para descargar el archivo solo si la contraseña es correcta
     with open(acumulado_excel_file, 'rb') as file:
         st.download_button(
             label="Descargar archivo Excel acumulado",
